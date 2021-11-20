b5db7a521720f8d5ab18775b22572f583ebefbfb
Luiz Rioja <lulyrioja@gmail.com>
2021-11-19 23:28:40 -0300

    :alien: Defining the application's theme and fonts

---
+++ src/styles/theme.ts
@@ -0,0 +1,18 @@
+export default {
+  colors: {
+    main: "#316BFF",
+    white: "##FFFFFF",
+    logout: "##DC434E",
+    background: "#FAFBFF",
+    main_light: "##EBF0FE",
+    text_primary: "#000000",
+    types_filter: "##F4F4F6",
+    text_secondary: "#8E94A3",
+  },
+
+  fonts: {
+    primary_700: "Muli_700Bold",
+    primary_300: "Muli_300Light",
+    primary_400: "Muli_400Regular",
+  },
+};
