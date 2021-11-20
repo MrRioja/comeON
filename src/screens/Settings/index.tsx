1d7fa3c483f75526dae7a7ba203d403b87c4a024
Luiz Rioja <lulyrioja@gmail.com>
2021-11-19 23:30:05 -0300

    :alien: Starting implementation and styling of Settings screen

---
+++ src/screens/Settings/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+
+import { Container } from "./styles";
+
+export function Settings() {
+  return (
+    <Container>
+      <Text>Settings Screen</Text>
+    </Container>
+  );
+}
