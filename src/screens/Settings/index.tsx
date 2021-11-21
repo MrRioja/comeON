940e327412c8ef1889733200bf104a3ccc661fd4
Luiz Rioja <lulyrioja@gmail.com>
2021-11-21 16:58:08 -0300

    :alien: Starting the implementation and styling of the screen settings

--- src/screens/Settings/index.tsx
+++ src/screens/Settings/index.tsx
@@ -1,12 +1,46 @@
 import React from "react";
-import { Text } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-import { Container } from "./styles";
+import theme from "../../styles/theme";
+import { Button } from "../../components/Button";
+
+import {
+  Container,
+  Title,
+  Header,
+  ProfileContainer,
+  ImageProfile,
+  Username,
+  UserLocation,
+  UserLocationContainer,
+} from "./styles";
 
 export function Settings() {
   return (
     <Container>
-      <Text>Settings Screen</Text>
+      <Header>
+        <Title>Settings</Title>
+
+        <Button name="log-out" color={theme.colors.logout} />
+      </Header>
+
+      <ProfileContainer>
+        <ImageProfile
+          source={{
+            uri: "https://images.pexels.com/photos/356147/pexels-photo-356147.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+          }}
+        />
+        <Username>Edmund Wernher</Username>
+
+        <UserLocationContainer>
+          <MaterialCommunityIcons
+            name="map-marker"
+            size={20}
+            color={theme.colors.main}
+          />
+          <UserLocation>Berlin, Germany</UserLocation>
+        </UserLocationContainer>
+      </ProfileContainer>
     </Container>
   );
 }
--- src/screens/Settings/styles.ts
+++ src/screens/Settings/styles.ts
@@ -1,7 +1,55 @@
 import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
 export const Container = styled.View`
   flex: 1;
+  background-color: ${({ theme }) => theme.colors.background};
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  height: 75px;
+  padding: 0px 24px;
+  flex-direction: row;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
+  margin-bottom: ${RFValue(50)}px;
+  margin-top: ${getStatusBarHeight() + 15}px;
+`;
+
+export const Title = styled.Text`
+  font-size: ${RFValue(35)}px;
+  font-family: ${({ theme }) => theme.fonts.primary_700};
+`;
+
+export const ProfileContainer = styled.View`
+  width: 100%;
+  align-items: center;
+`;
+
+export const ImageProfile = styled.Image`
+  width: 130px;
+  height: 130px;
+  margin-bottom: 10px;
+  border-radius: 30px;
+`;
+
+export const Username = styled.Text`
+  margin-bottom: 10px;
+  font-size: ${RFValue(25)}px;
+  font-family: ${({ theme }) => theme.fonts.primary_700};
+`;
+
+export const UserLocationContainer = styled.View`
+  padding: 10px 15px;
+  border-radius: 50px;
+  flex-direction: row;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.white};
+`;
+
+export const UserLocation = styled.Text`
+  margin-left: 5px;
+  font-family: ${({ theme }) => theme.fonts.primary_700};
 `;
