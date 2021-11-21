71d53ec571ff4b8559d70aad9f28d604ecefddd5
Luiz Rioja <lulyrioja@gmail.com>
2021-11-21 13:39:33 -0300

    :alien: Correcting color declaration

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,10 +1,10 @@
 export default {
   colors: {
     main: "#316BFF",
-    white: "##FFFFFF",
-    logout: "##DC434E",
+    white: "#FFFFFF",
+    logout: "#DC434E",
     background: "#FAFBFF",
-    main_light: "##EBF0FE",
+    main_light: "#EBF0FE",
     text_primary: "#000000",
     types_filter: "##F4F4F6",
     text_secondary: "#8E94A3",
