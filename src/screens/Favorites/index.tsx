69f9728dfe6f5be4569435d074cc9cb710c80c2b
Luiz Rioja <lulyrioja@gmail.com>
2021-11-27 23:03:45 -0300

    :alien: Creating the Favorites Screen

--- src/screens/Favorites/index.tsx
+++ src/screens/Favorites/index.tsx
@@ -1,12 +1,21 @@
 import React from "react";
-import { Text } from "react-native";
+import { Button } from "../../components/Button";
+import theme from "../../styles/theme";
 
-import { Container } from "./styles";
+import { FavoritesTabs } from "../../routes/app.topTab.routes";
+
+import { Container, Header, Title } from "./styles";
 
 export function Favorites() {
   return (
     <Container>
-      <Text>Favorite Screen</Text>
+      <Header>
+        <Title>Favorites</Title>
+
+        <Button name="search" color={theme.colors.text_primary} />
+      </Header>
+
+      <FavoritesTabs />
     </Container>
   );
 }
