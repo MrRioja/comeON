d48d5feb5f9e82b084632a18628f64ff576e4f4a
Luiz Rioja <lulyrioja@gmail.com>
2021-11-27 22:59:19 -0300

    :alien: Creating topTabs stack component

---
+++ src/routes/app.topTab.routes.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Dimensions } from "react-native";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+
+import { Sights } from "../screens/Sights";
+import { Tours } from "../screens/Tours";
+import { Adventures } from "../screens/Adventures";
+
+import theme from "../styles/theme";
+
+const Tab = createMaterialTopTabNavigator();
+
+export function FavoritesTabs() {
+  const w = Dimensions.get("window").width;
+  const dotSize = 7;
+
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: theme.colors.main,
+        tabBarPressColor: theme.colors.background,
+        tabBarInactiveTintColor: theme.colors.text_primary,
+        tabBarStyle: { backgroundColor: theme.colors.background },
+        tabBarIndicatorStyle: {
+          width: dotSize,
+          height: dotSize,
+          borderRadius: dotSize / 2,
+          left: (w / 3 - dotSize) / 2,
+          marginBottom: 3,
+        },
+      }}
+    >
+      <Tab.Screen name="Sights" component={Sights} />
+
+      <Tab.Screen name="Tours" component={Tours} />
+
+      <Tab.Screen name="Adventures" component={Adventures} />
+    </Tab.Navigator>
+  );
+}
