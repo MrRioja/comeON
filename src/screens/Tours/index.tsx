eba4895bce1899669e615c04e531c4b7ce0fe472
Luiz Rioja <lulyrioja@gmail.com>
2021-11-27 23:00:54 -0300

    :alien: Creating the Tours screen

---
+++ src/screens/Tours/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+
+import { Container } from "./styles";
+
+export function Tours() {
+  return (
+    <Container>
+      <Text>Tours</Text>
+    </Container>
+  );
+}
