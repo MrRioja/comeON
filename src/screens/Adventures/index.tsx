aec905d1dcc7219a07f669395210d028e2c1c637
Luiz Rioja <lulyrioja@gmail.com>
2021-11-27 22:59:55 -0300

    :alien: Creating the Adventures screen

---
+++ src/screens/Adventures/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+
+import { Container } from "./styles";
+
+export function Adventures() {
+  return (
+    <Container>
+      <Text>Adventures</Text>
+    </Container>
+  );
+}
