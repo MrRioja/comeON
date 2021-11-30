2c7848220ad8879c710979329d172022a7bd2ed6
Luiz Rioja <lulyrioja@gmail.com>
2021-11-30 00:33:50 -0300

    :alien: Starting the Search screen implementation and styling

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -1,12 +1,21 @@
 import React from "react";
-import { Text } from "react-native";
 
-import { Container } from "./styles";
+import { Container, Header, Title } from "./styles";
+import theme from "../../styles/theme";
+
+import { Button } from "../../components/Button";
+import { SearchBar } from "../../components/SearchBar";
 
 export function Search() {
   return (
     <Container>
-      <Text>Search Screen</Text>
+      <Header>
+        <Title>Search</Title>
+
+        <Button name="filter" color={theme.colors.text_primary} />
+      </Header>
+
+      <SearchBar placeholder="Where do you want to go?" />
     </Container>
   );
 }
--- src/screens/Search/styles.ts
+++ src/screens/Search/styles.ts
@@ -1,7 +1,22 @@
 import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
 
 export const Container = styled.View`
   flex: 1;
+  padding: 0px 24px;
+  background-color: ${({ theme }) => theme.colors.background};
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  flex-direction: row;
   align-items: center;
-  justify-content: center;
+  height: ${RFValue(75)}px;
+  margin-top: ${RFValue(25)}px;
+  justify-content: space-between;
+`;
+
+export const Title = styled.Text`
+  font-size: ${RFValue(35)}px;
+  font-family: ${({ theme }) => theme.fonts.primary_700};
 `;
