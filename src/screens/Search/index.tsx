16c971056a2664b0628edd060d71101c0ec6ca0c
Luiz Rioja <lulyrioja@gmail.com>
2021-12-03 23:14:15 -0300

    :alien: Fixing bug preventing FlatList scroll

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  Keyboard,
-  KeyboardAvoidingView,
-  TouchableWithoutFeedback,
-  View,
-} from "react-native";
+import { KeyboardAvoidingView, View } from "react-native";
 
 import { Container, Header, Title } from "./styles";
 import theme from "../../styles/theme";
@@ -17,27 +12,25 @@ import { ForYouPlaces } from "../../components/ForYouPlaces";
 export function Search() {
   return (
     <KeyboardAvoidingView behavior="height" style={{ flex: 1 }} enabled>
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <Container>
-          <Header>
-            <Title>Search</Title>
+      <Container>
+        <Header>
+          <Title>Search</Title>
 
-            <Button name="filter" color={theme.colors.text_primary} />
-          </Header>
+          <Button name="filter" color={theme.colors.text_primary} />
+        </Header>
 
-          <View style={{ paddingHorizontal: 24, marginBottom: 15 }}>
-            <SearchBar
-              placeholder="Where do you want to go?"
-              placeholderTextColor={theme.colors.text_secondary}
-              selectionColor={theme.colors.text_secondary}
-            />
-          </View>
+        <View style={{ paddingHorizontal: 24, marginBottom: 15 }}>
+          <SearchBar
+            placeholder="Where do you want to go?"
+            placeholderTextColor={theme.colors.text_secondary}
+            selectionColor={theme.colors.text_secondary}
+          />
+        </View>
 
-          <ForYouPlaces />
+        <ForYouPlaces />
 
-          <PopularPlaces />
-        </Container>
-      </TouchableWithoutFeedback>
+        <PopularPlaces />
+      </Container>
     </KeyboardAvoidingView>
   );
 }
