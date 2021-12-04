fe1dc0d6545665564c5b772dba8576424f0fc886
Luiz Rioja <lulyrioja@gmail.com>
2021-12-04 16:49:45 -0300

    :alien: Creating TopTab navigation from the Home screen

---
+++ src/routes/app.topTabExplore.routes.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Dimensions } from "react-native";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+
+import { SightsExplore } from "../screens/SightsExplore";
+import { ToursExplore } from "../screens/ToursExplore";
+import { AdventuresExplore } from "../screens/AdventuresExplore";
+
+import theme from "../styles/theme";
+import { RFValue } from "react-native-responsive-fontsize";
+
+const Tab = createMaterialTopTabNavigator();
+
+export function FavoritesTabs() {
+  const w = Dimensions.get("window").width;
+  const dotSize = 7;
+
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: theme.colors.main,
+        tabBarPressColor: theme.colors.background,
+        tabBarInactiveTintColor: theme.colors.text_primary,
+        tabBarStyle: {
+          backgroundColor: theme.colors.background,
+          elevation: 0,
+        },
+        tabBarLabelStyle: {
+          marginBottom: 10,
+          fontSize: RFValue(15),
+          textTransform: "capitalize",
+          fontFamily: theme.fonts.primary_400,
+        },
+        tabBarIndicatorStyle: {
+          width: dotSize,
+          height: dotSize,
+          borderRadius: dotSize / 2,
+          left: (w / 3 - dotSize) / 2,
+          marginBottom: 5,
+        },
+      }}
+    >
+      <Tab.Screen name="Sights" component={SightsExplore} />
+
+      <Tab.Screen name="Tours" component={ToursExplore} />
+
+      <Tab.Screen name="Adventures" component={AdventuresExplore} />
+    </Tab.Navigator>
+  );
+}
